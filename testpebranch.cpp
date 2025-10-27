c9b66f24ebdc5b247c762d70e7891b7a994c88ec
laurinho <ioan-laurentiu.nistor@student.unitbv.ro>
2025-10-27 21:52:45 +0200

    Test 2

--- testpebranch.cpp
+++ testpebranch.cpp
@@ -9,12 +9,6 @@
 #include <iostream>
 int main()
 {
-    std::cout<<"Laurinho";
-    std::cout<<"Hello, World!"<<std::endl;
-    std::cout<<"Ana"<<std::endl;
-    std::cout<<"Are"<<"Mere"<<std::endl;
-    std::cout<<"Ana are pere"<<std::endl;
-    std::cout<<"Mere"<<std::endl;
-    std::cout<<"Are"<<std::endl;
+
     return 0;
 }

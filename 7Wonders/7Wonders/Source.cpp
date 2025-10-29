9c60b673f372bfb18e2272c8c7aab88b6d677db1
laurinho <ioan-laurentiu.nistor@student.unitbv.ro>
2025-10-29 17:57:22 +0200

    Adaugare linii cod

--- 7Wonders/7Wonders/Source.cpp
+++ 7Wonders/7Wonders/Source.cpp
@@ -3,5 +3,11 @@ using namespace std;
 
 int main() {
     cout << "Hello, team!" << endl;
+	cout << "Welcome to the 7 Wonders project!" << endl;
+	cout << "This is a simple C++ program." << endl;
+	cout << "Let's build something amazing together!" << endl;
+	cout << "Good luck and have fun coding!" << endl;
+	cout << "This";
+
     return 0;
 }

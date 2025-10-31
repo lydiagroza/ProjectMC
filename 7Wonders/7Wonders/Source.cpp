441fbb63ddd6fa9cd37e5b5bf7e262c1bc7efefc
laurinho <ioan-laurentiu.nistor@student.unitbv.ro>
2025-10-31 20:02:58 +0200

    Inceput de implementare a clasei Player.

--- 7Wonders/7Wonders/Source.cpp
+++ 7Wonders/7Wonders/Source.cpp
@@ -2,12 +2,8 @@
 using namespace std;
 
 int main() {
-    cout << "Hello, team!" << endl;
-	cout << "Welcome to the 7 Wonders project!" << endl;
-	cout << "This is a simple C++ program." << endl;
-	cout << "Let's build something amazing together!" << endl;
-	cout << "Good luck and have fun coding!" << endl;
-	cout << "This";
+
+    
 
     return 0;
 }

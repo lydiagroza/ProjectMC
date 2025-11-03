394c145d346f7d2cfeeacfddb7ca9fb5f8cbdd53
laurinho <ioan-laurentiu.nistor@student.unitbv.ro>
2025-11-03 12:25:34 +0200

    Continuare metode clasa Player

--- 7Wonders/7Wonders/Player.cpp
+++ 7Wonders/7Wonders/Player.cpp
@@ -1,5 +1,5 @@
 #include"Player.h"
-
+#include<unordered_map>
 Player::Player(const std::string& playerName) :name(playerName), nrCoins(7), militaryScore(0), rawMaterials{}, manufacturedGoods{}, fixedCost{}, bonusProduction{}, cityBuildings{}, availableWonders{}, builtWonders{}, scientificSymbols{}, chainSymbols{} {
 }
 void Player::addCoins(int amount) {
@@ -12,6 +12,48 @@ bool Player::decreaseCoins(int amount) {
 	}
 	return false;
 }
+int Player::calculateTradeCost(const std::unordered_map < std::string, int>& requiredResurces, const Player& opponent)const {
+	int totalTradeCost = 0;
+	std::unordered_map<std::string, int>availableResurces = rawMaterials;
+	for(const auto&pair:manufacturedGoods) {
+		availableResurces[pair.first] += pair.second;
+	}
+	for(const auto&pair:bonusProduction) {
+		availableResurces[pair.first] += pair.second;
+	}
+	for (const auto& pair : requiredResurces) {
+		const std::string& resource = pair.first;
+		int requiredAmount = pair.second;
+		int missingAmount = requiredAmount - availableResurces[resource];
+		if (missingAmount > 0) {
+			if (fixedCost.count(resource)) {
+				totalTradeCost += missingAmount * fixedCost.at(resource);
+			}
+			else {
+				int opponentProduction = 0;
+				if (opponent.rawMaterials.count(resource)) {
+					opponentProduction += opponent.rawMaterials.at(resource);
+				}
+				if (opponent.manufacturedGoods.count(resource)) {
+					opponentProduction += opponent.manufacturedGoods.at(resource);
+				}
+				int costPerUnit = 2 + opponentProduction;
+				totalTradeCost += missingAmount * costPerUnit;
+			}
+			
+		}
+	}
+	return totalTradeCost;
+}
+bool Player::canAffordConstruction(int fixedCoinCost, const std::unordered_map<std::string, int>& requiredResources,const Player& opponent) {
+	int tradeCost = calculateTradeCost(requiredResources, opponent);
+	int totalCost = fixedCoinCost + tradeCost;
+	if(decreaseCoins(totalCost)) {
+		return true;
+	}
+	return false;
+ }
+
 //void Player::moveMilitaryScore(int shields,Player&opponent) {
 //	militaryScore += shields;
 //	if (militaryScore > 9) {
@@ -22,8 +64,6 @@ bool Player::decreaseCoins(int amount) {
 //	}
 //}
 
-//Player::addCards(const Card& card, Players& opponent, GameState& board) {
-//	cityBuildings[card.color].pushback(card);
-//}
+
 
 

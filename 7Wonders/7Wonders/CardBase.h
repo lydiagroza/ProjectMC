04e99abe8b46316292a67768390964dcc1b696bf
ale0050 <alexandramariamiron@gmail.com>
2025-11-03 12:45:10 +0200

    lumberyard

--- 7Wonders/7Wonders/CardBase.h
+++ 7Wonders/7Wonders/CardBase.h
@@ -1,15 +1,15 @@
-﻿#include <iostream>;
-#include <map>;
-#include <string>;
-#include<vector>;
-#include <optional>;
-#include<functional>;
-#include "Player.h"; 
-#include <cstdint>;
+﻿#include <iostream>
+#include <map>
+#include <string>
+#include<vector>
+#include <optional>
+#include<functional>
+#include "Player.h" 
+#include <cstdint>
 
 
   enum  Resource : std::uint8_t {
-        Wood, Clay, Stone, Glass, Papyrus, Gold
+        Wood, Clay, Stone, Glass, Papyrus, Coin
     };
 
   enum Color : std::uint8_t {
@@ -25,30 +25,31 @@
 	};
 
 
- class CardBase {
-    public:
-        std::string m_name;
-        std::uint16_t m_id : 16;
-        Color m_color : 3;
-        std::optional<Symbol> m_symbol;
-        std::optional<std::vector<Symbol>> m_unlocks;
-        std::map<Resource, std::uint8_t> m_cost;
-        CardBase();
-        CardBase(std::string name, std::uint16_t id, Color color,
-            const std::map<Resource, std::uint8_t>& cost,
-            const std::optional<Symbol>& symbol = std::nullopt,
-            const std::optional<std::vector<Symbol>>& unlocks = std::nullopt)
-            : m_name(std::move(name)), m_id(id), m_color(color),
-            m_cost(cost), m_symbol(symbol), m_unlocks(unlocks) {
-        }
-        const std::string& get_name() const;
-        std::uint16_t get_id() const;
-        Color get_color() const;
-        const std::map<Resource, std::uint8_t>& get_cost() const;
-        const std::optional<Symbol>& get_symbol() const;
-        const std::optional<std::vector<Symbol>>& get_unlocks() const;
-        void addEffect(std::function<void(Player&)> e) { efects.push_back(e); }
-        void applyEffects(Player& p) { for (auto& e : efects) e(p); }
+  class CardBase {
+  public:
+      std::string m_name;
+      std::uint16_t m_id : 16;
+      Color m_color : 3;
+      std::optional<Symbol> m_symbol;
+      std::optional<std::vector<Symbol>> m_unlocks;
+      std::map<Resource, std::uint8_t> m_cost;
+      std::vector<std::function<void(Player&)>> effects;
+      CardBase();
+      CardBase(std::string name, std::uint16_t id, Color color,
+          const std::map<Resource, std::uint8_t>& cost,
+          const std::optional<Symbol>& symbol = std::nullopt,
+          const std::optional<std::vector<Symbol>>& unlocks = std::nullopt)
+          : m_name(std::move(name)), m_id(id), m_color(color),
+          m_cost(cost), m_symbol(symbol), m_unlocks(unlocks) {
+      }
+      const std::string& get_name() const;
+      std::uint16_t get_id() const;
+      Color get_color() const;
+      const std::map<Resource, std::uint8_t>& get_cost() const;
+      const std::optional<Symbol>& get_symbol() const;
+      const std::optional<std::vector<Symbol>>& get_unlocks() const;
+      void applyEffect(Player& p);
+      void addEffect(std::function<void(Player&)> e);
     };
 
 
--- 7Wonders/7Wonders/HardcodareCarti.cpp
+++ 7Wonders/7Wonders/HardcodareCarti.cpp
@@ -1,134 +1,29 @@
-#include "CardBase.h"
-using namespace std;
+#include "Player.h"
+#include  "CardBase.h"
+#include <memory>
+#include <map>
+#include <vector>
 
-//std::vector<CardBase> createBrownCards() {
-//    std::vector<CardBase> cards;
+namespace wonders7 {
+  /*   m_name(std::move(name)), m_id(id), m_color(color),
+        m_cost(cost), m_symbol(symbol), m_unlocks(unlocks)*/
 
-//    // Age I - Brown Cards
-//    {
-//        CardBase c;
-//        c.m_id = 1;
-//        c.m_name = "Lumber Yard";
-//        c.m_color = Color::Brown;
-//        c.m_cost = {};
-//        c.m_symbol = std::nullopt;
-//        c.m_unlocks = std::nullopt;
-//        cards.push_back(c);
-//    }
-//    {
-//        CardBase c;
-//        c.m_id = 2;
-//        c.m_name = "Logging Camp";
-//        c.m_color = Color::Brown;
-//        c.m_cost = { {Resource::Wood, 1} };
-//        c.m_symbol = std::nullopt;
-//        c.m_unlocks = std::nullopt;
-//        cards.push_back(c);
-//    }
-//    {
-//        CardBase c;
-//        c.m_id = 3;
-//        c.m_name = "Clay Pool";
-//        c.m_color = Color::Brown;
-//        c.m_cost = {};
-//        c.m_symbol = std::nullopt;
-//        c.m_unlocks = std::nullopt;
-//        cards.push_back(c);
-//    }
-//    {
-//        CardBase c;
-//        c.m_id = 4;
-//        c.m_name = "Clay Pit";
-//        c.m_color = Color::Brown;
-//        c.m_cost = { {Resource::Clay, 1} };
-//        c.m_symbol = std::nullopt;
-//        c.m_unlocks = std::nullopt;
-//        cards.push_back(c);
-//    }
-//    {
-//        CardBase c;
-//        c.m_id = 5;
-//        c.m_name = "Quarry";
-//        c.m_color = Color::Brown;
-//        c.m_cost = {};
-//        c.m_symbol = std::nullopt;
-//        c.m_unlocks = std::nullopt;
-//        cards.push_back(c);
-//    }
-//    {
-//        CardBase c;
-//        c.m_id = 6;
-//        c.m_name = "Stone Pit";
-//        c.m_color = Color::Brown;
-//        c.m_cost = { {Resource::Stone, 1} };
-//        c.m_symbol = std::nullopt;
-//        c.m_unlocks = std::nullopt;
-//        cards.push_back(c);
-//    }
-//
-//    // Age II - Brown Cards
-//    {
-//        CardBase c;
-//        c.m_id = 7;
-//        c.m_name = "Sawmill";
-//        c.m_color = Color::Brown;
-//        c.m_cost = { {Resource::Wood, 2} };
-//        c.m_symbol = std::nullopt;
-//        c.m_unlocks = std::nullopt;
-//        cards.push_back(c);
-//    }
-//    {
-//        CardBase c;
-//        c.m_id = 8;
-//        c.m_name = "Brickyard";
-//        c.m_color = Color::Brown;
-//        c.m_cost = { {Resource::Clay, 2} };
-//        c.m_symbol = std::nullopt;
-//        c.m_unlocks = std::nullopt;
-//        cards.push_back(c);
-//    }
-//    {
-//        CardBase c;
-//        c.m_id = 9;
-//        c.m_name = "Shelf Quarry";
-//        c.m_color = Color::Brown;
-//        c.m_cost = { {Resource::Stone, 2} };
-//        c.m_symbol = std::nullopt;
-//        c.m_unlocks = std::nullopt;
-//        cards.push_back(c);
-//    }
-//    {
-//        CardBase c;
-//        c.m_id = 10;
-//        c.m_name = "Glass Blower";
-//        c.m_color = Color::Brown; // technically Gray, but in image appears as refined resource
-//        c.m_cost = {};
-//        c.m_symbol = std::nullopt;
-//        c.m_unlocks = std::nullopt;
-//        cards.push_back(c);
-//    }
-//
-//    return cards;
-//}
+    std::vector<std::unique_ptr<CardBase>> createBrownCards() {
+        std::vector<std::unique_ptr<CardBase>> cards;
+        auto c = std::make_unique<CardBase>(
+            "Lumber Yard",
+            1,
+            Color::Brown,
+            std::map<Resource, std::uint8_t>{{Resource::Wood, 1}}, // cost 
+            std::nullopt, // fara simbol 
+            std::nullopt  // fara unlock 
+        );
+          c->addEffect([](Player& p) {
+            p.addResource(Resource::Wood, 1);
+            });
 
-import CardBase;
-import <iostream>;
+        cards.push_back(std::move(c));
+        return cards;
+    }
 
-using namespace wonders7;
-
-int main() {
-    CardBase c1("Baths", 1, Color::Blue,
-        { {Resource::Stone, 1} },
-        Symbol::Lamp);
-
-    CardBase c2("Guard Tower", 2, Color::Red,
-        { {Resource::Clay, 1} },
-        Symbol::Sword);
-
-    CardBase c3("Marketplace", 3, Color::Yellow,
-        { {Resource::Gold, 2} },
-        std::nullopt,
-        std::vector<Symbol>{Symbol::Gear, Symbol::Scroll});
-
-    std::cout << c1 << c2 << c3;
 }

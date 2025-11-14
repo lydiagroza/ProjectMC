6ddcf107952468e7f399988e299dca229a2d5878
Alexandra <aleandreea006@gmail.com>
2025-11-14 12:59:58 +0200

    am adaugat o metoda care va calulca cum se schimba pozitia pionului pe tabla si ce fect are aceasta miscare asupra token urilor si a banilor, respectiv daca conduce la victorie militara

--- 7Wonders/7Wonders/MilitaryTrack.h
+++ 7Wonders/7Wonders/MilitaryTrack.h
@@ -14,10 +14,16 @@ class MilitaryTrack
 private:
     
 	int8_t m_pawnPosition = 0; 
+    static constexpr int8_t MAX_Position = 9;
+
+	bool m_removedTokens[4] = { false, false, false, false };
+	// player 1 token 5, player 1 token 2, player 2 token 2, player 2 token 5
 
 public:
     MilitaryTrack() = default;
 
 	int getPawnPosition() const;
+
+	MilitaryResult applyShields(int platerId, int shields);
 };
 

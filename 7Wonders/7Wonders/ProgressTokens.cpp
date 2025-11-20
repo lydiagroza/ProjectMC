86280be72747d982bc229a6df48b00a1d5b24ab3
lydiagroza <131545909+lydiagroza@users.noreply.github.com>
2025-11-20 18:38:37 +0200

    Comments

--- 7Wonders/7Wonders/ProgressTokens.cpp
+++ 7Wonders/7Wonders/ProgressTokens.cpp
@@ -4,7 +4,7 @@
 #include <iostream>
 #include "CardBase.h"
 
-// Forward declaration to avoid circular dependency
+
 class Player;
 
 std::vector<std::function<void(Player&, Player&)>> ProgressToken::parseEffects(const std::string& s) {
@@ -15,19 +15,20 @@ std::vector<std::function<void(Player&, Player&)>> ProgressToken::parseEffects(c
     static const std::unordered_map<std::string, std::function<void(Player&, Player&)>> effectMap = {
         {"add_coins6", [](Player& p, Player& o) { p.addCoins(6); }},
         {"add_VictoryPoint4", [](Player& p, Player& o) { p.add_Points(Points::Victory, 4); }},
-        {"WondersDiscount", [](Player& p, Player& o) { /* Placeholder */ std::cout << "WondersDiscount not implemented." << std::endl; }},
-        {"gainOpponentTradeCost", [](Player& p, Player& o) { /* Placeholder */ std::cout << "gainOpponentTradeCost not implemented." << std::endl; }},
+        {"WondersDiscount", [](Player& p, Player& o) { /* WondersDiscount */ std::cout << "WondersDiscount not implemented." << std::endl; }},
+        {"gainOpponentTradeCost", [](Player& p, Player& o) {/* gainOpponentTradeCost */ std::cout << "gainOpponentTradeCost not implemented." << std::endl; }},
         {"add_scientific_symbol_law", [](Player& p, Player& o) { p.add_ScientificSymbol(Scientific_Symbol::Scales); }},
-        {"victoryPointsPerProgress", [](Player& p, Player& o) { /* Placeholder */ std::cout << "victoryPointsPerProgress not implemented." << std::endl; }},
-        {"BlueCardDiscount", [](Player& p, Player& o) { /* Placeholder */ std::cout << "BlueCardDiscount not implemented." << std::endl; }},
+        {"victoryPointsPerProgress", [](Player& p, Player& o) { /* victoryPointsPerProgress */ std::cout << "victoryPointsPerProgress not implemented." << std::endl; }},
+        {"BlueCardDiscount", [](Player& p, Player& o) { /* BlueCardDiscount */ std::cout << "BlueCardDiscount not implemented." << std::endl; }},
         {"add_VictoryPoint7", [](Player& p, Player& o) { p.add_Points(Points::Victory, 7); }},
-        {"add_MilitaryPoint1PerRedCard", [](Player& p, Player& o) { /* Placeholder */ std::cout << "add_MilitaryPoint1PerRedCard not implemented." << std::endl; }},
-        {"WondersGetReplayTurn", [](Player& p, Player& o) { /* Placeholder */ std::cout << "WondersGetReplayTurn not implemented." << std::endl; }},
-        {"add_coins4PerFreeCard", [](Player& p, Player& o) { /* Placeholder */ std::cout << "add_coins4PerFreeCard not implemented." << std::endl; }}
+        /* aici la Military trebuie sa vedem cum facem, daca returnam cumva nr de carti spre board sau le adaugam undeva*/
+        {"add_MilitaryPoint1PerRedCard", [](Player& p, Player& o) { /* add_MilitaryPoint1PerRedCard */ std::cout << "add_MilitaryPoint1PerRedCard not implemented." << std::endl; }},
+        {"WondersGetReplayTurn", [](Player& p, Player& o) { /* WondersGetReplayTurn */ std::cout << "WondersGetReplayTurn not implemented." << std::endl; }},
+        {"add_coins4PerFreeCard", [](Player& p, Player& o) { /* add_coins4PerFreeCard */ std::cout << "add_coins4PerFreeCard not implemented." << std::endl; }}
     };
 
     while (std::getline(ss, effectStr, ';')) {
-        // Trim whitespace and quotes
+       
         effectStr.erase(remove_if(effectStr.begin(), effectStr.end(), ::isspace), effectStr.end());
         effectStr.erase(remove(effectStr.begin(), effectStr.end(), '"'), effectStr.end());
 

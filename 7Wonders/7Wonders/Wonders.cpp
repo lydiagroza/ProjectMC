a0bb635ea52ee97ef1ffee355ee257e02ef5c77c
lydiagroza <131545909+lydiagroza@users.noreply.github.com>
2025-11-20 17:53:39 +0200

    WonderLoader.cpp - refacut Effecte la Wonder.cpp Wonder.h - refacut

--- 7Wonders/7Wonders/Wonders.cpp
+++ 7Wonders/7Wonders/Wonders.cpp
@@ -1,19 +1,69 @@
-#include "CardBase.h";
-#include<iostream>;
-#include <format>; 
-#include <vector>;
+#include "Wonder.h"
+#include <sstream>
+#include <unordered_map>
+#include <iostream>
 
+class Player;
 
-	const std::string& CardBase::get_name() const {
-		return m_name;
-	}
+std::map<Resource, uint8_t> Wonder::parseCost(const std::string& s) {
+    std::map<Resource, uint8_t> cost;
+    std::stringstream ss(s);
+    std::string item;
+    while (std::getline(ss, item, ';')) {
+        std::string resourceName;
+        int amount = 0;
+        for (char c : item) {
+            if (isalpha(c)) {
+                resourceName += c;
+            } else if (isdigit(c)) {
+                amount = amount * 10 + (c - '0');
+            }
+        }
 
-	std::uint16_t CardBase::get_id() const {
-		return m_id;
-	}
+        if (amount == 0) amount = 1; // Default la 1 daca nu este specificat un amount
 
-	Points Cardbase::getPoints() const {
-		return m_points;
-	}
+        if (resourceName == "wood") cost[Resource::Wood] = amount;
+        else if (resourceName == "clay") cost[Resource::Clay] = amount;
+        else if (resourceName == "stone") cost[Resource::Stone] = amount;
+        else if (resourceName == "glass") cost[Resource::Glass] = amount;
+        else if (resourceName == "papyrus") cost[Resource::Papyrus] = amount;
+        else if (resourceName == "coin") cost[Resource::Coin] = amount;
+    }
+    return cost;
+}
 
+std::vector<std::function<void(Player&, Player&)>> Wonder::parseEffects(const std::string& s) {
+    std::vector<std::function<void(Player&, Player&)>> effects;
+    std::stringstream ss(s);
+    std::string effectStr;
 
+    static const std::unordered_map<std::string, std::function<void(Player&, Player&)>> effectMap = {
+        {"add_coins3", [](Player& p, Player& o) { p.addCoins(3); }},
+        {"decreaseCoins3", [](Player& p, Player& o) { o.decreaseCoins(3); }},
+        {"replayTurn", [](Player& p, Player& o) { /* replayturn */ std::cout << "Replay turn effect not implemented." << std::endl; }},
+        {"add_VictoryPoint3", [](Player& p, Player& o) { p.add_Points(Points::Victory, 3); }},
+        {"wood/stone/clay", [](Player& p, Player& o) { p.add_Resource(Resource::Wood, 1); p.add_Resource(Resource::Stone, 1); p.add_Resource(Resource::Clay, 1); }},
+        {"add_VictoryPoints4", [](Player& p, Player& o) { p.add_Points(Points::Victory, 4); }},
+        {"add_coins12", [](Player& p, Player& o) { p.addCoins(12); }},
+        {"discardOpponentGrayCard", [](Player& p, Player& o) { /* discardgraycard */ std::cout << "Discard opponent gray card not implemented." << std::endl; }},
+        {"add_MilitaryPoint1", [](Player& p, Player& o) { p.add_Points(Points::Military, 1); }},
+        {"buildDiscardedCard", [](Player& p, Player& o) { /* BuilDiscardedCard */ std::cout << "Build discarded card not implemented." << std::endl; }},
+        {"add_VictoryPoint2", [](Player& p, Player& o) { p.add_Points(Points::Victory, 2); }},
+        {"papyrus/glass", [](Player& p, Player& o) { p.add_Resource(Resource::Papyrus, 1); p.add_Resource(Resource::Glass, 1); }},
+        {"add_VictoryPoints9", [](Player& p, Player& o) { p.add_Points(Points::Victory, 9); }},
+        {"chooseProgressToken", [](Player& p, Player& o) {/*ChooseProgressToken */ std::cout << "Choose progress token not implemented." << std::endl; }},
+        {"add_coins6", [](Player& p, Player& o) { p.addCoins(6); }},
+        {"add_VictoryPoints", [](Player& p, Player& o) { p.add_Points(Points::Victory, 1); }},
+        {"add_MilitaryPoints2", [](Player& p, Player& o) { p.add_Points(Points::Military, 2); }},
+        {"discardOpponentBrownCard", [](Player& p, Player& o) { /* DiscardOpponentBrownCard */ std::cout << "Discard opponent brown card not implemented." << std::endl; }}
+    };
+
+    while (std::getline(ss, effectStr, ';')) {
+        if (effectMap.count(effectStr)) {
+            effects.push_back(effectMap.at(effectStr));
+        } else {
+            std::cerr << "Unknown effect: " << effectStr << std::endl;
+        }
+    }
+    return effects;
+}

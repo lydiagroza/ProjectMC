c44cca27fc1a95841020f8e183de564e89ae0df0
Alexandra <aleandreea006@gmail.com>
2025-12-06 17:36:04 +0200

    afiseaza interfata acum...stiu ca arata naspa dar o sa lucrez la asta

--- 7Wonders/7Wonders/MainWindow.h
+++ 7Wonders/7Wonders/MainWindow.h
@@ -43,6 +43,12 @@ private:
     QPushButton* militaryPawn;
     std::vector<QPushButton*> progressTokens;
 
+    // --- HEADER ---
+    QLabel* titleLabel;
+    QLabel* ageLabel; 
+    QPushButton* startButton; // Buton de start joc
+    QPushButton* nextAgeButton;
+
     // --- FUNCȚII ---
     void setupRightPanel();
     void updateMilitaryPawn();

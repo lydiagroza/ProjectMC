e3c31d3123b64048f1bad459941d31c115f7ccb9
Alexandra <aleandreea006@gmail.com>
2026-01-12 23:08:21 +0200

    incerc sa fac ceva interfata

---
+++ 7Wonders/gameUI/CardWidget.cpp
@@ -0,0 +1,18 @@
+﻿#include "CardWidget.h"
+
+CardWidget::CardWidget(int cardId, QWidget* parent)
+    : QPushButton(parent), m_cardId(cardId)
+{
+    // Setări vizuale de bază (opțional)
+    this->setFixedSize(100, 150); // Dimensiunea unei cărți în pixeli
+    this->setStyleSheet("border: 1px solid black;"); // Un contur simplu
+}
+
+void CardWidget::setCardImage(const QString& imagePath)
+{
+    // Aici încărcăm imaginea pe buton
+    QPixmap pixmap(imagePath);
+    QIcon buttonIcon(pixmap);
+    this->setIcon(buttonIcon);
+    this->setIconSize(this->size());
+}

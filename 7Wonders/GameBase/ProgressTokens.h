8d175ccdba710d62e25275a401e3bdb3020ebe0b
lydiagroza <131545909+lydiagroza@users.noreply.github.com>
2026-01-14 15:11:09 +0200

    test

---
+++ 7Wonders/GameBase/ProgressTokens.h
@@ -0,0 +1,27 @@
+#pragma once
+#include <string>
+#include <functional>
+#include <cstdint>
+#include "gameExport.h" // <-- 1. Include the export header
+
+class Player;
+
+class GAME_API ProgressToken {
+    std::string  m_name;
+    std::uint16_t id;
+    std::vector<std::function<void(Player&, Player&)>> effects;
+
+public:
+    ProgressToken(const std::string& name, std::uint16_t id)
+        : m_name(name), id(id) {
+    }
+
+
+    const std::string& getName() const;
+    std::uint16_t getId() const { return id; }
+    const std::vector<std::function<void(Player&, Player&)>>& getEffects() const;
+
+    void addEffect(std::function<void(Player&, Player&)> e);
+};
+
+std::ostream& operator<<(std::ostream& os, const ProgressToken& token);

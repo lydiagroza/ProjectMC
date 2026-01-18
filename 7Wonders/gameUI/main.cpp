41e4782a4ae439e08fe7c4e06d732ac802de7310
lydiagroza <131545909+lydiagroza@users.noreply.github.com>
2026-01-18 16:32:43 +0200

    Revert "commit"
    This reverts commit 8b8520b9b6bc9cfdecd6e41cac4d8b0ed394081c.

--- 7Wonders/gameUI/main.cpp
+++ 7Wonders/gameUI/main.cpp
@@ -1,38 +1,10 @@
 ﻿#include "MainWindow.h"
 #include <QtWidgets/QApplication>
-#include <QFile>
-#include <QDebug>
-#include <QImageReader>
-#include <QDir>
-#include <QDirIterator>
 
 int main(int argc, char* argv[])
 {
     // 1. Inițializează aplicația Qt (gestionează resursele grafice)
     QApplication a(argc, argv);
-
-    qDebug() << "--- RESOURCE DEBUG ---";
-    qDebug() << "Checking :/background.jpg:" << QFile::exists(":/background.jpg");
-    qDebug() << "Checking :/resources/UI/quack.mp3:" << QFile::exists(":/resources/UI/quack.mp3");
-    
-    qDebug() << "Supported Image Formats:";
-    for(auto fmt : QImageReader::supportedImageFormats()) {
-        qDebug() << fmt;
-    }
-
-    qDebug() << "Library Paths:" << QApplication::libraryPaths();
-    
-    QDir dir(":/");
-    qDebug() << "Root Resource Contents:";
-    for(auto entry : dir.entryList()) {
-        qDebug() << " - " << entry;
-    }
-    QDir resDir(":/resources/UI");
-    qDebug() << "Resources/UI Contents:";
-    for(auto entry : resDir.entryList()) {
-        qDebug() << " - " << entry;
-    }
-    qDebug() << "----------------------";
 
     // 2. Creează fereastra principală (care conține Board-ul și Jocul)
     MainWindow w;
